8a6aa2a7fa8dd9ac694425d282a9f9a2de5bdd61
Bunny Pranav <pranav.vasipalli@gmail.com>
2021-01-17 20:56:59 +0530

    Added bot

--- Bunny's-Bot.py
+++ Bunny's-Bot.py
@@ -36,4 +36,18 @@ async def hyd(ctx):
     await ctx.send('The website link for Hydrerabad is **rebrand.ly/hydtodo**')
 
 
+@bot.command
+async def helppy(ctx):
+    embed = discord.Embed(
+        tittle='discord.py Help websites',
+        description='Help for discord.py with different websites',
+        colour=discord.Color.blue())
+    embed.set_author(name="Bunny Pranav",
+                     url="https://rebrand.ly/bunny-website", icon_url=ctx.author.avatar_url)
+    embed.set_author(name="Bunny Pranav",
+                     url="https://rebrand.ly/bunny-website", icon_url=ctx.author.avatar_url)
+    embed.set_thumbnail(url='https://bit.ly/3styBZX')
+    embed.add_field(
+        name='Bot Embed', value='https://medium.com/python-in-plain-english/send-an-embed-with-a-discord-bot-in-python-61d34c711046')
+
 bot.run('Nzk4MTk4MzYxMDY0NjAzNzEx.X_xiJw.JRqsIZ3lV1SvuhGEuKaMrOJEo4Q')

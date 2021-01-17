b3d6870d7c35924f0c4bdafb243693c240053423
Bunny Pranav <pranav.vasipalli@gmail.com>
2021-01-17 21:46:47 +0530

    Added embed feature

--- Bunny's-Bot.py
+++ Bunny's-Bot.py
@@ -11,7 +11,7 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.say(f'Hello! I am there and my latency is {round(bot.latency*1000)}ms ')
+    await ctx.send(f'Hello! I am there and my latency is {round(bot.latency*1000)}ms ')
 
 
 @bot.command()
@@ -31,17 +31,17 @@ async def math(ctx, oper, ni, ns):
         await ctx.send(int(ni)/int(ns))
 
 
-@bot.command
+@bot.command()
 async def hyd(ctx):
-    await ctx.send('The website link for Hydrerabad is **rebrand.ly/hydtodo**')
+    embed = discord.Embed(title="Hydrerabad To-Do", url="https://rebrand.ly/hydtodo",
+                          description="The website link for Hydrerabad To-Do is **rebrand.ly/hydtodo**", color=discord.Color.blue())
+    await ctx.send(embed=embed)
 
 
-@bot.command
+@bot.command()
 async def helppy(ctx):
     embed = discord.Embed(
-        tittle='discord.py Help websites',
-        description='Help for discord.py with different websites',
-        colour=discord.Color.blue())
+        tittle='discord.py Help websites', description='Help for discord.py with different websites', colour=discord.Color.blue())
     embed.set_author(name="Bunny Pranav",
                      url="https://rebrand.ly/bunny-website", icon_url=ctx.author.avatar_url)
     embed.set_author(name="Bunny Pranav",
@@ -49,5 +49,8 @@ async def helppy(ctx):
     embed.set_thumbnail(url='https://bit.ly/3styBZX')
     embed.add_field(
         name='Bot Embed', value='https://medium.com/python-in-plain-english/send-an-embed-with-a-discord-bot-in-python-61d34c711046')
+    embed.add_field(name='Repl.it Cloud',
+                    value='https://repl.it/@BunnyPranav/Bunnys-Bot#main.py')
+    await ctx.send(embed=embed)
 
 bot.run('Nzk4MTk4MzYxMDY0NjAzNzEx.X_xiJw.JRqsIZ3lV1SvuhGEuKaMrOJEo4Q')
